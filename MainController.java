81207e1617a04cf01e2fc0f3bb44d29ee091f4c2
jimlorm <jimlormmartinez@gmail.com>
2026-08-02 18:33:33 +0800

    feat: add an entry

--- MainController.java
+++ MainController.java
@@ -1,5 +1,12 @@
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.ChoiceDialog;
+import java.util.Optional;
 
 public class MainController {
 
@@ -22,7 +29,12 @@ public class MainController {
             }
         });
 
+        // Listen for delete entry button
         btnDelete.setOnAction(event -> handleDeleteEntry());
+
+        // Listen for add entry button
+        btnAdd.setOnAction(event -> handleAddEntry());
+
     }
 
     public void setLibrary(Library library) {
@@ -66,5 +78,67 @@ public class MainController {
             errorAlert.setContentText("Please select an entry to delete.");
             errorAlert.showAndWait();
         }
+    }
+
+    private void handleAddEntry() {
+        Stage addWindow = new Stage();
+        addWindow.initModality(Modality.APPLICATION_MODAL);
+        addWindow.setTitle("Add New Media");
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(15));
+
+        ComboBox<String> typeBox = new ComboBox<>();
+        typeBox.getItems().addAll("Book", "Movie", "TV Series");
+        typeBox.setValue("Book");
+
+        TextField titleField = new TextField();
+        titleField.setPromptText("Enter Title");
+
+        TextField genreField = new TextField();
+        genreField.setPromptText("Enter Genre");
+
+        TextField extra1Field = new TextField();
+        extra1Field.setPromptText("Author (Book) / Director (Movie) / Episodes (TV)");
+
+        TextField extra2Field = new TextField();
+        extra2Field.setPromptText("Pages (Book) / Runtime (Movie) / Leave empty for TV");
+
+        Button submitButton = new Button("Save Entry");
+
+        submitButton.setOnAction(e -> {
+            try {
+                String type = typeBox.getValue();
+                String title = titleField.getText();
+                String genre = genreField.getText();
+
+                if (type.equals("Book")) {
+                    int pages = Integer.parseInt(extra2Field.getText());
+                    library.addEntry(new Book(title, genre, Status.PLANNED, extra1Field.getText(), pages));
+                } else if (type.equals("Movie")) {
+                    int runtime = Integer.parseInt(extra2Field.getText());
+                    library.addEntry(new Movie(title, genre, Status.PLANNED, extra1Field.getText(), runtime));
+                } else if (type.equals("TV Series")) {
+                    int episodes = Integer.parseInt(extra1Field.getText());
+                    library.addEntry(new TVSeries(title, genre, Status.PLANNED, episodes));
+                }
+
+                refreshList();
+                addWindow.close();
+
+            } catch (NumberFormatException ex) {
+                Alert error = new Alert(Alert.AlertType.ERROR, "Please enter valid numbers for Pages/Runtime/Episodes!");
+                error.showAndWait();
+            }
+        });
+
+        layout.getChildren().addAll(
+                new Label("Select Media Type:"), typeBox,
+                titleField, genreField, extra1Field, extra2Field,
+                submitButton
+        );
+
+        addWindow.setScene(new Scene(layout, 300, 250));
+        addWindow.showAndWait();
     }
 }
