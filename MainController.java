1069cb4e70789f3454f9eaa44ef6d7b328f30c2f
jimlorm <jimlormmartinez@gmail.com>
2026-08-02 18:09:03 +0800

    feat: delete an entry

--- MainController.java
+++ MainController.java
@@ -21,6 +21,8 @@ public class MainController {
                 }
             }
         });
+
+        btnDelete.setOnAction(event -> handleDeleteEntry());
     }
 
     public void setLibrary(Library library) {
@@ -35,6 +37,34 @@ public class MainController {
             for (MediaEntry m : library.getAllMedia()) {
                 mediaListView.getItems().add(m.getTitle() + " (" + m.getCurrentStatus() + ")");
             }
+        }
+    }
+
+    private void handleDeleteEntry() {
+        // Get the currently selected item's index
+        int selectedIndex = mediaListView.getSelectionModel().getSelectedIndex();
+
+        if (selectedIndex >= 0) {
+            // Confirm deletion with a pop-up
+            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            confirmAlert.setTitle("Confirm Delete");
+            confirmAlert.setHeaderText(null);
+            confirmAlert.setContentText("Are you sure you want to delete this entry?");
+
+            // If the user clicks ok, delete it from the Model and refresh the View
+            confirmAlert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    library.deleteEntry(selectedIndex); // Delete from backend
+                    refreshList(); // Update the visual list
+                    detailsTextArea.setText("Select an item to see details..."); // Clear the text area
+                }
+            });
+        } else {
+            // If they clicked the button without selecting an item first
+            Alert errorAlert = new Alert(Alert.AlertType.WARNING);
+            errorAlert.setHeaderText(null);
+            errorAlert.setContentText("Please select an entry to delete.");
+            errorAlert.showAndWait();
         }
     }
 }
