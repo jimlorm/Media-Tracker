a08a438b83025d5bc7841263a7cd44f86a52396f
jimlorm <jimlormmartinez@gmail.com>
2026-08-04 09:17:11 +0800

    add module

--- module-info.java
+++ module-info.java
@@ -1,9 +1,7 @@
 module mediavault {
-    // These tell Java that we require the JavaFX libraries
     requires javafx.controls;
     requires javafx.fxml;
 
-    // This grants JavaFX permission to read your FXML and Controller files
     opens mediavault to javafx.fxml;
     exports mediavault;
 }
